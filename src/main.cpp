2fa3ce1e89814864ecb0ef373d86fb3d83c7b974
Walid Amriou <walidamriou@gmail.com>
2020-09-06 20:10:56 +0100

    added the main code

--- src/main.cpp
+++ src/main.cpp
@@ -12,10 +12,21 @@ Last update: September 2020
 
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#include <IRremote.h>
+
+const int RECV_PIN = 7;
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+
+void setup(){
+  Serial.begin(115200);
+  irrecv.enableIRIn();
+  irrecv.blink13(true);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop(){
+  if (irrecv.decode(&results)){
+        Serial.println(results.value, HEX);
+        irrecv.resume();
+  }
 }
