e0478dc4a5b25111d846bad9529fc4cea1d12273
Walid Amriou <walidamriou@gmail.com>
2020-09-06 19:42:17 +0100

    added template

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+/* 
+Arduino_IR_receiver
+Implement IR receiver for Arduino 
+Developed by: Walid Amriou
+www.walidamriou.com
+
+This work is licensed under:
+Creative Commons Attribution-NonCommercial-NoDerivatives 4.0 International License.
+
+Last update: September 2020
+*/
+
+#include <Arduino.h>
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
